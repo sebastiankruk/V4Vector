7f09d9b0e0eb28e0b201b4528371f931e2f79b25
Kruk, Sebastian <dev@kruk.me>
2019-06-13 20:44:14 +0200

    async find humans

--- main.py
+++ main.py
@@ -19,11 +19,14 @@ Created by Sebastian Kruk and Michał Szopiński
 """
 
 import sys
+import time
+import sched
 import getopt
 import threading
 import datetime
 import inflect
 import random
+import traceback
 
 import anki_vector
 from anki_vector.events import Events
@@ -48,116 +51,125 @@ class V4Vector(object):
     self.vector = vector
     self.jira = JiraChecker()
     self.inflect_engine = inflect.engine()
+    self.scheduler = sched.scheduler(time.time, time.sleep)
+    self.robot = None
+
+    self.scheduler.enter(30, 1, self.__find_faces)
 
     print("Initialized")
 
-
-  def __find_faces(self, robot):
+  def __find_faces(self):
     '''
     Extended version of the find faces algoritm
     '''
-    actions = random.choices(('drive', 'head', 'turn'), k=random.randint(1,3))
-
+    # found_faces = len(self.detected_faces-V4Vector.RANDOM_CITIZEN_SET)
+    # timeout = 60 if found_faces > 0 else 30
+    actions = random.choices(('head', 'turn'), k=random.randint(1,3)) #'drive', 
     print(f"------ Vector will look for faces {actions}! ------")
+    self.detected_faces.clear()
+    self.robot.behavior.drive_off_charger()
 
     while actions:
-      if 'head' in actions:
-        robot.behavior.set_head_angle(degrees(random.randint(0, 45)))
-      if 'drive' in actions:
-        robot.behavior.drive_straight(distance_mm(random.randint(50, 100)), speed_mmps(100))
-      if 'turn' in actions:
-        robot.behavior.drive_off_charger()
-        robot.behavior.turn_in_place(degrees(random.randint(-90, 90)))
+      action = actions.pop()
+      if 'head' in action:
+        self.robot.behavior.set_head_angle(degrees(random.randint(40, 45)))
+      if 'drive' in action:
+        self.robot.behavior.drive_straight(distance_mm(random.randint(-10, 10)), speed_mmps(100))
+      if 'turn' in action:
+        self.robot.behavior.turn_in_place(degrees(random.randint(25, 30)))
       #find_faces
       #look_around_in_place
     
 
   def run(self):
-      self.said_text = False
       self.detected_faces = set()
 
       def on_robot_observed_face(robot, event_type, event, evt):
-        self.said_text
-        if not self.said_text:
+        print(f"Vector sees a face {datetime.datetime.now()} {event_type}")
 
-          # print(f"Vector sees a face {datetime.datetime.now()}")
+        faces = list()
+        for face in robot.world.visible_faces:
+          faces.append(face)
 
-          faces = list()
-          for face in robot.world.visible_faces:
-            faces.append(face)
+        for face in faces:
 
-          for face in faces:
+          face_name = face.name if face.name is not '' else V4Vector.RANDOM_CITIZEN
 
-            if not face.name in self.detected_faces:
+          if face_name not in self.detected_faces:
+            try:
+              if face.expression is Expression.HAPPINESS.value:
+                emotion = 'happy'
+                positive_emotion = True
+              elif face.expression is Expression.SURPRISE.value:
+                emotion = 'surprised'  
+                positive_emotion = True
+              elif face.expression is Expression.ANGER.value:
+                emotion = 'angry'
+                positive_emotion = False
+              elif face.expression is Expression.SADNESS.value:
+                emotion = 'sad'
+                positive_emotion = False
+              else:
+                emotion = ''
+                positive_emotion = None
 
-              try:
-                if face.expression is Expression.HAPPINESS.value:
-                  emotion = 'are happy'
-                elif face.expression is Expression.ANGER.value:
-                  emotion = 'are angry'
-                elif face.expression is Expression.SADNESS.value:
-                  emotion = 'are sad'
-                elif face.expression is Expression.SURPRISE.value:
-                  emotion = 'are surprised'  
-                else:
-                  emotion = ''
+              # face_message = f"I see you {emotion}, {face_name}!"
+              # robot.behavior.say_text(face_message)
+              self.detected_faces.add(face_name)
 
-                face_name = face.name if face.name is not '' else RANDOM_CITIZEN
-                face_message = f"I see you {emotion}, {face_name}!"
-                print(face_memessage)
-                robot.behavior.say_text(face_memessage)
-                self.detected_faces.add(face_name)
-
-                if face.name is not '':
-                  jira_tickets = self.jira.check_tickets_for_user(face.name, face.time_since_last_seen)
-                  jira_tickets_total = jira_tickets['total']
-                  if jira_tickets_total < 1:
-                    robot.behavior.say_text(f"You have no new jira tickets")
+              if face.name is not '':
+                jira_tickets = self.jira.check_tickets_for_user(face.name, face.time_since_last_seen)
+                jira_tickets_total = jira_tickets['total']
+                if jira_tickets_total < 1:
+                  if positive_emotion is True:
+                    joiner = f', is that why you are {emotion}?'
+                  elif positive_emotion is False:
+                    joiner = f', so why are you {emotion}?'
                   else:
-                    robot.behavior.say_text(f"You have {jira_tickets_total} jira { self.inflect_engine.plural('ticket', jira_tickets_total) }")
+                    joiner = '!'
+                  robot.behavior.say_text(f"{face_name}, you have no new jira tickets{joiner}")
+                else:
+                  if positive_emotion is False:
+                    joiner = f', is that why you are {emotion}?'
+                  elif positive_emotion is True:
+                    joiner = f', so why are you {emotion}?'
+                  else:
+                    joiner = '!'
+                  robot.behavior.say_text(f"{face_name}, you have {jira_tickets_total} jira { self.inflect_engine.plural('ticket', jira_tickets_total) }{joiner}")
 
-                  for ticket in jira_tickets['issues']:
-                    robot.behavior.say_text(f"{ticket['summary']}")
+                for idx, ticket in enumerate(jira_tickets['issues']):
+                  robot.behavior.say_text(f"Issue number {idx}: {ticket['summary']}")
 
-                # self.said_text = True
-                # print(f"Face: {face.name}, {face.expression}")
-              except Exception as e:
-                print(e)
+            except:
+              print(traceback.format_exc())
+              e = sys.exc_info()[0]
+              print("Exception in handling face detection")
+              print(e)
+          else:
+            print(face_name)
+        # evt.set()
 
-          # evt.set()
-
-      with anki_vector.Robot(enable_face_detection=True, behavior_control_level=ControlPriorityLevel.OVERRIDE_BEHAVIORS_PRIORITY) as robot:
+      with anki_vector.Robot(enable_face_detection=True) as robot:
+        self.robot = robot
         robot.vision.enable_face_detection(estimate_expression=True)
         robot.vision.enable_display_camera_feed_on_face()
-
-        # If necessary, move Vector's Head and Lift to make it easy to see his face
         robot.behavior.set_head_angle(degrees(45.0))
         robot.behavior.set_lift_height(0.0)
-
 
         evt = threading.Event()
         robot.events.subscribe(on_robot_observed_face, Events.robot_observed_face, evt)
 
-        # + wake_word
-        # + user_intent => https://sdk-resources.anki.com/vector/docs/generated/anki_vector.user_intent.html
-
         print("------ waiting for face events, press ctrl+c to exit early ------")
+        
+        self.scheduler.run()
 
         try:
-          timeout = 15
-
-          while True:
-            if not evt.wait(timeout=timeout):
-              self.__find_faces(robot)
-              self.detected_faces.clear()
-
-            found_faces = len(self.detected_faces-RANDOM_CITIZEN_SET)
-            timeout = 60 if found_faces else 10
-
+          if not evt.wait(timeout=12000):
+            print("----- that's all folks ------")
         except KeyboardInterrupt:
           pass
 
-      robot.events.unsubscribe(on_robot_observed_face, Events.robot_observed_face)
+        robot.events.unsubscribe(on_robot_observed_face, Events.robot_observed_face)
 
 
 
